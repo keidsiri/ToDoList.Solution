f0bc7de60fd882fcff05d7fe5d6a74057763e842
Kate Kiatsiri <keidsiri@gmail.com>
2021-12-13 14:29:42 -0800

    Add folders and files for web application instead of console

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,9 +1,26 @@
+using System; // directive - tells it what code it needs in order to function.
+using System.Collections.Generic; // if using list objects
+
 namespace ToDoList.Models
 {
   public class Item
   {
+    public string Description {get; set;}
+    private static List<Item> _instances = new List<Item> {};
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
 
-    // properties, methods, etc. will go here.
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
 
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
